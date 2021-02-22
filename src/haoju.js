6c1e2805d7aad3b1f6a6d506f5e9db61df1e3ecd
fly6022 <2875752791@qq.com>
2021-02-22 19:24:45 +0800

    2021-02-22-01

--- src/haoju.js
+++ src/haoju.js
@@ -12,11 +12,18 @@ function gethaojudata() {
      js.type = "text/javascript";   // 引入的文件类型
      document.getElementsByTagName("body")[0].appendChild(js)  // 引入到HTML标签
 
+}
+
+/*function gethaojucss() { 
+
     var css = document.createElement("link"); 
      css.href = "https://fonts.googleapis.com/css?family=Noto+Serif+SC:200,300,400,500,600,700,900";
      css.rel = "stylesheet";
      document.getElementsByTagName("head")[0].appendChild(css)
 
 }
+*/
 
-setTimeout("gethaojudata()","0"); // 调用函数
+setTimeout("gethaojudata()","0"); // 调用数据源
+
+/* setTimeout("gethaojucss()","0"); // 调用CSS */
