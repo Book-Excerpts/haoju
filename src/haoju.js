51f22a81f81e6c8d8b5ea991e696888ca030d04d
fly6022 <2875752791@qq.com>
2021-03-06 22:56:50 +0800

    v3.0.1

--- src/haoju.js
+++ src/haoju.js
@@ -1,14 +1,20 @@
-﻿/*『书摘。』好句插件 by fly6022*/
+/*『书摘。』好句插件 v3.0.0 by fly6022*/
 /* 本插件开源遵循 MIT 协议 https://book.fly6022.fun/ */
 
-function getdata () {
-    var url = ["/src/haoju/1.json"]; // 数据库
+website = ""; // 请省略结尾的“/”。
+
+window.onload = function getdata1 () { // 数据请求函数1(采用XMLHttpRequest方法）
+
+    var url = [website + "/src/haoju/1.json"]; // 数据库JSON文件路径
+
     var request = new XMLHttpRequest();
-        request.open("GET", url); // 采用GET方式
+        request.open("GET", url); // 采用GET方式请求数据
         request.send(null);
         request.onload = function() {
             if (request.status == 200) {
                 hdata = eval(JSON.parse(request.responseText));
+                setTimeout("dealdata()","0");
+                setInterval("dealdata()","15000"); // 调用数据源
                 console.info("书摘 v3.0.0 (https://book.fly6022.fun/)");
                 }
             if (request.status == 404){
@@ -17,14 +23,8 @@ function getdata () {
             }
     }
 
-window.onload = getdata();
-setTimeout("getdata()","0");
-
-function dealdata () {
+function dealdata () { // 数据处理函数
     var len=538;// 好句数量（赋值应小于等于数据库数据总量，否则会报错）
     var rnd=Math.floor(Math.random()*len);// 根据好句数量生成随机值
     document.getElementById("haoju").innerHTML = "<center>" + "“" + hdata.haoju_1[rnd].haoju + "”" + "<br>——" + hdata.haoju_1[rnd].author + "&nbsp;" + hdata.haoju_1[rnd].from + "</center>";
 }
-
-setTimeout("dealdata()","50");
-setInterval("dealdata()","15000"); // 调用数据源
