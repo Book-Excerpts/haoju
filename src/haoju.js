27c0b4cf31d5645887057487a52140e4eb334e24
fly6022 <2875752791@qq.com>
2021-03-06 12:38:24 +0800

    v3.0.0

--- src/haoju.js
+++ src/haoju.js
@@ -1,17 +1,20 @@
 ﻿/*『书摘。』好句插件 by fly6022*/
 /* 本插件开源遵循 MIT 协议 https://book.fly6022.fun/ */
 
+var website = "";
+
 function getdata () {
-    var url = ["/src/haoju/1.json"]; // 数据库
+    var url = [website + "/src/haoju/1.json"]; // 数据库
     var request = new XMLHttpRequest();
         request.open("GET", url); // 采用GET方式
         request.send(null);
         request.onload = function() {
             if (request.status == 200) {
                 hdata = eval(JSON.parse(request.responseText));
+                console.info("书摘 v3.0.0 (https://book.fly6022.fun/)");
                 }
-            else {
-                console.error("ERROR!请求书摘数据库时发生错误！")
+            if (request.status == 404){
+                console.error("ERROR!请求书摘数据库时发生错误！");
                 }
             }
     }
@@ -20,7 +23,7 @@ window.onload = getdata();
 setTimeout("getdata()","0");
 
 function dealdata () {
-    var len=538;// 好句数量（小于等于数据库数据总量）
+    var len=538;// 好句数量（赋值应小于等于数据库数据总量，否则会报错）
     var rnd=Math.floor(Math.random()*len);// 根据好句数量生成随机值
     document.getElementById("haoju").innerHTML = "<center>" + "“" + hdata.haoju_1[rnd].haoju + "”" + "<br>——" + hdata.haoju_1[rnd].author + "&nbsp;" + hdata.haoju_1[rnd].from + "</center>";
 }
